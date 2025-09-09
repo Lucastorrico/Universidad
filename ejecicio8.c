36e0c73a80cf184e4210e1556f517408a3bc6924
Lucastorrico <146089522+Lucastorrico@users.noreply.github.com>
2025-09-09 13:55:27 -0300

    Add files via upload

---
+++ ejecicio8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+   int a,b,c;
+   printf("ingrese el valor de a: \n");
+   scanf("%d", &a);
+   printf("ingrese el valor de b: \n");
+   scanf("%d", &b);
+   printf("ingrese el valor de c: \n");
+   scanf("%d", &c);
+   printf("la formula es: %dx^2 %dx %d",a,b,c);
+
+}
